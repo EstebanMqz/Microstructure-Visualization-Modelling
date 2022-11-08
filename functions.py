1e9134b9124c5a03c75f34931764f820ce5d2c31
Esteban Márquez Delgado <emarquez1895@gmail.com>
2022-11-07 19:19:35 -0600

    Functions and Notebook Modularity (up to OHLCV)
    functions find_exchanges, cctx_download (criptos and exchanges retrieval verif.)

--- functions.py
+++ functions.py
@@ -9,27 +9,115 @@
 # -- ------------------------------------------------------------------------------------------------------------------------   -- #
 """
 
-from dataclasses import dataclass
 import pandas as pd
 import pandas_datareader as pdr
-from lib2to3.pgen2.grammar import opmap_raw
+import ccxt #Criptocurrencies
+import logging as log
 import numpy as np
 from data import *
 
-def f_leer_archivo(param_archivo):
-    """
-    Function that reads csv files and returns a dataframe of its content.
 
-        Parameters
+def find_exchanges(features=None, is_authenticated=False):
+    """
+    Function that returns avaliable cryptocurrencies exchanges in Python for CCTX module.
+
+        Parameters:
         ----------
-        param_archivo: csv data.
-        Returns
+        features: None 
+        is_authenticated: None 
+
+        Returns:
         -------
-        data: pd.DataFrame(param_archivo)
+        exchange_names: Array of authenticated exchanges avaliable in CCTX.
+
     """
+    ccxt_features = []
+    if features is not None:
+        for feature in features:
+            if not feature.endswith('Bundle'):
+                ccxt_features.append(feature)
 
-    df = pd.read_csv(param_archivo)
-    return df
+    exchange_names = []
+    for exchange_name in ccxt.exchanges:
+        if is_authenticated:
+            exchange_auth = get_exchange_auth(exchange_name)
+
+            has_auth = (exchange_auth['key'] != ''
+                        and exchange_auth['secret'] != '')
+
+            if not has_auth:
+                continue
+
+        log.debug('loading exchange: {}'.format(exchange_name))
+        exchange = getattr(ccxt, exchange_name)()
+
+        if ccxt_features is None:
+            has_feature = True
+
+        else:
+            try:
+                has_feature = all(
+                    [exchange.has[feature] for feature in ccxt_features]
+                )
+
+            except Exception:
+                has_feature = False
+
+        if has_feature:
+            try:
+                log.info('initializing {}'.format(exchange_name))
+                exchange_names.append(exchange_name)
+
+            except Exception as e:
+                log.warn(
+                    'unable to initialize exchange {}: {}'.format(
+                        exchange_name, e
+                    )
+                )
+
+    return exchange_names 
 
 
+def cctx_download(lvls, cripto, exchange):
+    """
+    Function that returns prices and quantities of given levels (as integer) Bids & Asks in an Orderbook
+    for the specified criptocurrency ('BTC/USDT','ETH/USDT','XRP/USDT', or others) and
+    from the following exchanges: binance, ftx or ascendex (as string).
+
+        Parameters:
+        ----------
+        lvls: Levels of bids/asks in the Order Book (int).
+        cripto: Criptocurrency downloadable ticker (str).
+        exchanges: Criptocurrency downloadable Exchange (str).
+
+        Returns:
+        -------
+        levels_ob_bid: Prices and Quantities of Bids (pos [0]) and Asks (pos [1]) as dataframes.
+    """
+    if exchange == 'binance':
+        binance = ccxt.binance()
+        levels_ob = binance.fetch_order_book(cripto, limit=lvls) #Order Books 
+        levels_ob_bid = pd.DataFrame(levels_ob['bids'], columns = ['price','quantity']) #Levels and Qt
+        levels_ob_ask = pd.DataFrame(levels_ob['asks'], columns = ['price','quantity']) 
+        levels_ob_bid.index.name = 'Bid_Lvl' 
+        levels_ob_ask.index.name = 'Ask_Lvl'   
+
+    elif exchange == 'ftx':
+        ftx = ccxt.ftx()
+        levels_ob = ftx.fetch_order_book(cripto, limit=lvls) #Order Books 
+        levels_ob_bid = pd.DataFrame(levels_ob['bids'], columns = ['price','quantity']) #Levels and Qt
+        levels_ob_ask = pd.DataFrame(levels_ob['asks'], columns = ['price','quantity']) 
+        levels_ob_bid.index.name = 'Bid_Lvl' 
+        levels_ob_ask.index.name = 'Ask_Lvl' 
+
+    elif exchange == 'ascendex':
+        ascendex = ccxt.ascendex()
+        levels_ob = ascendex.fetch_order_book(cripto, limit=lvls) #Order Books 
+        levels_ob_bid = pd.DataFrame(levels_ob['bids'], columns = ['price','quantity']) #Levels and Qt
+        levels_ob_ask = pd.DataFrame(levels_ob['asks'], columns = ['price','quantity']) 
+        levels_ob_bid.index.name = 'Bid_Lvl' 
+        levels_ob_ask.index.name = 'Ask_Lvl'      
+
+    return levels_ob_bid, levels_ob_ask
+    
 
